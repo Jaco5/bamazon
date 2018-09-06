e212ea68b8c4bc440093510e8ecbbb270e52313c
JMA <jacobartz1987@gmail.com>
2018-09-06 13:24:03 -0600

    Actual final commit

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 See a list of items in a DB to purchase from. 
 View list, enter item ID, enter qty, the list updates.
+
+Pic of bash terminal:
+https://docs.google.com/document/d/1In6z45NFVmRzJ27uiIXvTOJ9nO7-bQxYnYN3R8TD-Fg/edit?usp=sharing
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,19 +10,6 @@ var connection = mysql.createConnection({
     password: "root",
     database: "bamazon"
 });
-//This function will print the items in stock
-function showStock() {
-    var line = "\n--------------------------------";
-    console.log("\nWhat would you like to purchase?\n" + line);
-    connection.query("SELECT * FROM forsale", function(err, res) {
-        if (err) throw err;
-        res.forEach(function(item) {
-            console.log(item.item_name + " Item ID: " + item.item_id + ", $" + item.price + "\n" + item.stock + " in stock" + "\nDepartment: " + item.department_name + line);
-        });
-        console.log("\n");
-      promptUser();
-    });
-};
 
 function promptUser() {
     inquirer.prompt([
@@ -37,37 +24,51 @@ function promptUser() {
             name: "units"
         }
     ])
-    .then(function(response) {
-        connection.query("SELECT * FROM forsale WHERE item_id=" + response.id, function(err, res) {
-            if (err) throw err;
+        .then(function (response) {
+            connection.query("SELECT * FROM forsale WHERE item_id=" + response.id, function (err, res) {
+                if (err) throw err;
 
-            var itemStock = res[0].stock;
-            var unitPrice = res[0].price;
-            if (response.units <= itemStock) {
-                var modStock = itemStock - response.units;
-                connection.query("UPDATE forsale SET ? WHERE ?", 
-                [
-                    {
-                        stock: modStock
-                    },
-                    {
-                        item_id: response.id
-                    }
-                ], function(err, res) {
-                    if (err) throw err;
-                    var totalCost = response.units * unitPrice;
-                    console.log("Your total cost is $" + totalCost + ".");
+                var itemStock = res[0].stock;
+                var unitPrice = res[0].price;
+                if (response.units <= itemStock) {
+                    var modStock = itemStock - response.units;
+                    connection.query("UPDATE forsale SET ? WHERE ?",
+                        [
+                            {
+                                stock: modStock
+                            },
+                            {
+                                item_id: response.id
+                            }
+                        ], function (err, res) {
+                            if (err) throw err;
+                            var totalCost = response.units * unitPrice;
+                            console.log("Your total cost is $" + totalCost + ".");
+                            showStock();
+                        })
+                } else {
+                    console.log("THIS IS OUT OF STOCK.");
                     showStock();
-                })
-            } else {
-                console.log("THIS IS OUT OF STOCK.");
-                showStock();
-            }
+                }
+            })
         })
-    })
 }
 
-connection.connect(function(err) {
+//This function will print the items in stock
+function showStock() {
+    var line = "\n--------------------------------";
+    console.log("\nWhat would you like to purchase?\n" + line);
+    connection.query("SELECT * FROM forsale", function (err, res) {
+        if (err) throw err;
+        res.forEach(function (item) {
+            console.log(item.item_name + " Item ID: " + item.item_id + ", $" + item.price + "\n" + item.stock + " in stock" + "\nDepartment: " + item.department_name + line);
+        });
+        console.log("\n");
+        promptUser();
+    });
+};
+
+connection.connect(function (err) {
     if (err) throw err;
     showStock();
 });
